34cb038bc08e3aa5aa7a8f1986c208f4248397e7
Vidal Dz <admin@always>
2018-09-18 22:47:13 -0700

    initial setup for the punchoOutEnv

--- punchout-ai/punchout_ai/envs/punchoutEnv.py
+++ punchout-ai/punchout_ai/envs/punchoutEnv.py
@@ -6,9 +6,14 @@ class punchoutAIEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
+    self._observation=[]
     self.observation_space = spaces.Dict({
         "self_health": spaces.Discrete(255),
         "opponent_health": spaces.Discrete(255),
+        "opponent_action": spaces.Discrete(255),
+        "in_move": spaces.Discrete(2),
+        "round_over": spaces.Discrete(2),
+        "result": spaces.Discrete(2),
     })
 
     self.action_space = spaces.MultiDiscrete([
@@ -16,8 +21,10 @@ class punchoutAIEnv(gym.Env):
         [0,6], #None, A, B, X, Y, L ,R
     ])
 
+    self.seed()
+
   def step(self, action):
-    pass
+    self._observation = self._compute_observation(action)
 
   def reset(self):
     pass
@@ -27,3 +34,13 @@ class punchoutAIEnv(gym.Env):
 
   def render(self, mode='human', close=False):
     pass
+    
+  def _compute_observation(self, action):
+    return spaces.Dict({
+        "self_health": action.p1.health,
+        "opponent_health": action.p2.health,
+        "opponent_action": action.p2.action,
+        "in_move": action.p2.InMove,
+        "round_over": action.round_over,
+        "result": action.result,
+    })
